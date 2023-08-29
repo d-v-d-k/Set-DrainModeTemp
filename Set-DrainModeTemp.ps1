b2fed5d8ca414d03901c80710ccaf703c3893719
Dan van der Krieke <36768779+d-v-d-k@users.noreply.github.com>
2023-08-29 05:38:15 +0200

    Add files via upload

---
+++ Set-DrainModeTemp.ps1
@@ -0,0 +1,17 @@
+﻿param([Parameter(Mandatory=$true)][int]$Hours)
+
+if((cmd.exe /c chglogon.exe /query '2>&1') -eq "Session logins are currently ENABLED")
+{
+    chglogon.exe /drain
+
+    $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddHours($Hours)
+    $action = New-ScheduledTaskAction -Execute "chglogon.exe" -Argument "/enable"
+    $settings = New-ScheduledTaskSettingsSet -Compatibility V1 -DeleteExpiredTaskAfter (New-TimeSpan -Seconds 0)
+    Register-ScheduledTask -TaskName "Drain Mode - Enable" -Trigger $trigger -Action $action -Settings $settings -User "SYSTEM" -Force
+
+    Write-Host "Remote Desktop sessions set to drain, will be enabled in $Hours hours."
+}
+else
+{
+    Write-Host "Remote Desktop sessions are not enabled, skipping script."
+}
